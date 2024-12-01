d856944afe3999384ce4b36409304a6e95821012
1pa1sincamiseta <rolagonza22@gmail.com>
2024-12-01 03:27:55 -0500

    Configurando el hitbox y forma del jugador

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from player import Player
+from circleshape import CircleShape
 
 def main():
 	print("Stating asteroids!")
@@ -8,6 +10,12 @@ def main():
 
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+	clock = pygame.time.Clock()
+	dt = 0
+ 
+	player.draw(screen)
 
 	while True:
 		for event in pygame.event.get():
@@ -15,7 +23,13 @@ def main():
 				return
 
 		screen.fill("black")
+  
+		player.draw(screen)
+		
 		pygame.display.flip()
+
+		dt = clock.tick(60) / 1000
+		
 
 if __name__ == "__main__":
     main()
